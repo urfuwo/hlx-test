52ecdda32927a60b4182243f63ebbe605b2b167f
oliver <o.kohl@sap.com>
2024-01-16 13:48:05 +0100

    push push

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -18,4 +18,33 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+  const next = document.createElement('div');
+  next.innerHTML = `<udex-text-field
+  label="Test"
+  value
+  supporting-text="Test"
+  value-state="Standard"
+  direction="left"
+  maxlength="60"
+></udex-text-field>
+
+<br /> <br />
+
+<udex-hero-banner
+  background-image="/images/bg3.png"
+  background-image-label="Hero Banner background image label"
+  background-color="#fbdab9"
+  id="showcase"
+>
+    <div slot="content" class="hero-banner hero-banner--showcase">
+      <h1>Grow your business through enterprise</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere in massa id fringilla. Nunc rutrum turpis purus, et dignissim arcu gravida ut. Morbi sed sem ac magna consectetur tincidunt dapibus sed ipsum. </p>
+      <div class="hero-banner__buttons">
+        <ui5-button design="Positive">Request a demo</ui5-button>
+        <ui5-button design="Transparent">Watch the video</ui5-button>
+      </div>
+    </div>
+</udex-hero-banner>`;
+
+  block.append(next);
 }
