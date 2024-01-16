bc9a93d6edba7c087898f6eb6a29b05203275860
Patrick Hupe <hupe@adobe.com>
2024-01-16 16:07:36 +0100

    fix: hero image first version

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,25 +3,19 @@
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // decorate hero DOM
+  // fetch optimized image
   const image = block.querySelector('img');
   const imageSrc = image.getAttribute('src').replace('format=jpeg', 'format=webply');
   const hero = document.createElement('div');
   hero.innerHTML = `<udex-hero-banner
-  background-image="${imageSrc}"
-  background-image-label="Hero Banner background image label"
-  background-color="#fbdab9"
-  id="showcase"
->
+    background-image="${imageSrc}"
+    background-image-label="Hero Banner background image label"
+    background-color="#fbdab9"
+    id="showcase"
+  >
     <div slot="content" class="hero-banner hero-banner--showcase">
-      <h1>Grow your business through enterprise</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere in massa id fringilla. Nunc rutrum turpis purus, et dignissim arcu gravida ut. Morbi sed sem ac magna consectetur tincidunt dapibus sed ipsum. </p>
-      <div class="hero-banner__buttons">
-        <ui5-button design="Positive">Request a demo</ui5-button>
-        <ui5-button design="Transparent">Watch the video</ui5-button>
-      </div>
     </div>
-</udex-hero-banner>`;
-
+  </udex-hero-banner>`;
+  hero.querySelector('.hero-banner').append(block.querySelector('h1'));
   block.replaceWith(hero);
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import {
 
 const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
 const TEMPLATE_LIST = {
-    blog: 'blog',
+  blog: 'blog',
 };
 
 /**
