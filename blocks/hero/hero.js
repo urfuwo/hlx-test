e09ad1e8122a035e3c94b425052f94b93915dd28
oliver <o.kohl@sap.com>
2024-01-16 14:50:01 +0100

    hero push

---
+++ blocks/hero/hero.js
@@ -0,0 +1,25 @@
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  // decorate hero DOM
+  const hero = document.createElement('div');
+  hero.innerHTML = `<udex-hero-banner
+  background-image="/images/bg3.png"
+  background-image-label="Hero Banner background image label"
+  background-color="#fbdab9"
+  id="showcase"
+>
+    <div slot="content" class="hero-banner hero-banner--showcase">
+      <h1>Grow your business through enterprise</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere in massa id fringilla. Nunc rutrum turpis purus, et dignissim arcu gravida ut. Morbi sed sem ac magna consectetur tincidunt dapibus sed ipsum. </p>
+      <div class="hero-banner__buttons">
+        <ui5-button design="Positive">Request a demo</ui5-button>
+        <ui5-button design="Transparent">Watch the video</ui5-button>
+      </div>
+    </div>
+</udex-hero-banner>`;
+
+  block.replaceWith(hero);
+}
