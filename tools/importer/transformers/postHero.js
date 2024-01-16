6c9e159043f6f56385fda4306d3fe90fc8ad1505
Markus Haack <mhaack@adobe.com>
2024-01-16 15:43:20 +0100

    chore: support hero variants

--- tools/importer/transformers/postHero.js
+++ tools/importer/transformers/postHero.js
@@ -1,14 +1,25 @@
+/* global WebImporter */
 const transformHero = (main, document) => {
   const hero = document.querySelector('.c-hero ');
   if (hero) {
     // clean up hero stuff
     hero.querySelectorAll('.social-share-list, .c-post-type, .c-entry-meta').forEach((el) => el.remove());
 
+    // map hero content
+    const content = hero.querySelector('article');
+    let blockName = 'Hero';
+    if (content.classList.contains('has-light-overlay')) blockName = 'Hero (light overlay)';
+    if (content.classList.contains('has-dark-overlay')) blockName = 'Hero (dark overlay)';
+
     // unwrap hero image
-    const heroImage = hero.querySelector('a.c-post-link-wrapper img');
+    const heroImage = content.querySelector('a.c-post-link-wrapper img');
     if (heroImage) {
       heroImage.closest('a').replaceWith(heroImage);
     }
+
+    const block = [[blockName], [content.innerHTML]];
+    const table = WebImporter.DOMUtils.createTable(block, document);
+    hero.replaceWith(table);
   }
 };
 export default transformHero;
