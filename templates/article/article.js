e4e59f443b11ef15e4ccd2f0a3acb51f2551d6fa
Patrick Hupe <hupe@adobe.com>
2024-01-17 14:27:31 +0100

    fix: highlight blog page initial paragraph

--- templates/article/article.js
+++ templates/article/article.js
@@ -1,6 +1,27 @@
-import { buildBlock, decorateBlock, decorateSections } from '../../scripts/aem.js';
+import {
+  buildBlock,
+  decorateBlock,
+  decorateSections,
+  getMetadata,
+} from '../../scripts/aem.js';
+
+/**
+ * Highlight the first paragraph of a blog page
+ * Only enabled for pages annotated as metadata:template = blog
+ * @param main
+ */
+function highlightBlogFirstParagraph(main) {
+  if (getMetadata('template') === 'blog') {
+    const firstPara = main.querySelector('p');
+    if (firstPara) {
+      firstPara.classList.add('blog--highlight');
+    }
+  }
+}
 
 export default function decorateMain(main) {
+  highlightBlogFirstParagraph(main);
+
   const div = document.createElement('div');
   div.classList.add('section', 'article-footer');
 
