9f16c2a1bf9c9fd314955aa92d71aec9ada7f240
Ben Peter <benp@adobe.com>
2024-01-18 20:13:27 +0100

    decorate video links
    * decorate video links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -64,6 +64,38 @@ async function loadTemplateStyles() {
   }
 }
 
+async function decorateVideoLinks(main) {
+  const embedYoutube = (url, autoplay = false) => {
+    const usp = new URLSearchParams(url.search);
+    const suffix = autoplay ? '&muted=1&autoplay=1' : '';
+    let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
+    const embed = url.pathname;
+    if (url.origin.includes('youtu.be')) {
+      [, vid] = url.pathname.split('/');
+    }
+    const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+      </div>`;
+    return embedHTML;
+  };
+
+  const videoPs = main.querySelectorAll('p a[href*="youtu"]');
+  videoPs.forEach((a) => {
+    const videoP = a.parentNode;
+    const link = a.href;
+    videoP.textContent = '';
+    const observer = new IntersectionObserver((entries) => {
+      if (entries.some((e) => e.isIntersecting)) {
+        observer.disconnect();
+        videoP.innerHTML = embedYoutube(new URL(link), false);
+        videoP.classList.add('video');
+      }
+    });
+    observer.observe(videoP);
+  });
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -76,6 +108,7 @@ export async function decorateMain(main) {
   await decorateTemplates(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateVideoLinks(main);
 }
 
 async function loadSAPTheme() {
