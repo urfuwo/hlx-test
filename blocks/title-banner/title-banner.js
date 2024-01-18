b3540e8054846a916d7a390b73b6f7c99fbe46d0
Ben Peter <benp@adobe.com>
2024-01-18 23:08:28 +0100

    fill title-banner.author from metadata if empty

---
+++ blocks/title-banner/title-banner.js
@@ -0,0 +1,14 @@
+import { getMetadata } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  if (block.classList.contains('author')) {
+    if (!block.querySelector('h1')) {
+      const author = getMetadata('author');
+      if (author) {
+        const h1 = document.createElement('h1');
+        h1.innerHTML = `Articles by <strong>${author}</strong>`;
+        block.prepend(h1);
+      }
+    }
+  }
+}
