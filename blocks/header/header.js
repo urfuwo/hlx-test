ee0d6b1f15c04f52c0978e05e1e0e2c12e42c227
Saurabh Khare <skhare@adobe.com>
2024-01-26 23:01:59 +0100

    Added mobile layout for master header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,5 +1,8 @@
-import { getMetadata } from '../../scripts/aem.js';
+import { getMetadata, decorateIcons } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import {
+  div, span, input, p, button
+} from '../../scripts/dom-builder.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
@@ -61,7 +64,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null
     ? !forceExpanded
     : nav.getAttribute('aria-expanded') === 'true';
-  const button = nav.querySelector('.nav-hamburger button');
+  const button = nav.querySelector('.burger-menu');
   document.body.style.overflowY = expanded || isDesktop.matches ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
   toggleAllNavSections(
@@ -98,6 +101,69 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
+function addSearchBar(element) {
+  const searchBar = div(
+    { class: 'search-bar' },
+    div(
+      { class: 'text-box' },
+      span({ class: 'label-text' }),
+      div({ class: 'voice-search' }, div({ class: 'microphone' })),
+      div({ class: 'search' }, div({ class: 'search-e' })),
+    ),
+    input({ class: 'home-search-field-input', placeholder: 'Search' }),
+  );
+  element.prepend(searchBar);
+}
+
+function createMenu(nav) {
+  const navBar = div({ class: 'nav-bar' });
+  const classes = ['sections', 'explore', 'tools'];
+  classes.forEach((c) => {
+    const section = nav.querySelector(`.${c}`);
+    if (section) {
+      navBar.append(section);
+      section.remove();
+    }
+  });
+  nav.append(navBar);
+}
+
+function getActionBar(nav, navSections) {
+  const openButton = p(
+    { class: 'button-container burger-menu' },
+    button(
+      {
+        class: 'button',
+        type: 'button',
+        'aria-controls': 'nav',
+        'aria-label': 'Open Navigation',
+      },
+      span({ class: 'icon icon-burger' }),
+    ),
+  );
+  openButton.addEventListener('click', () => toggleMenu(nav, navSections));
+  const closeButton = p(
+    { class: 'button-container close-menu' },
+    button(
+      {
+        class: 'button',
+        type: 'button',
+        'aria-controls': 'nav',
+        'aria-label': 'Close Navigation',
+      },
+      span({ class: 'icon icon-close' }),
+    ),
+  );
+
+  const separator = div({ class: 'separator' });
+
+  const actionBar = div({ class: 'nav-action' });
+  actionBar.append(openButton);
+  actionBar.append(separator);
+  actionBar.append(closeButton);
+  return actionBar;
+}
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -111,15 +177,14 @@ export default async function decorate(block) {
   const nav = document.createElement('nav');
   nav.id = 'nav';
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
-
-  const classes = ['brand', 'sections', 'tools'];
+  const classes = ['brand', 'sections', 'explore', 'tools'];
   classes.forEach((c, i) => {
     const section = nav.children[i];
     if (section) section.classList.add(`nav-${c}`);
   });
-
   // populate logo
   const navBrand = nav.querySelector('.nav-brand');
+  const navBar = div({ class: 'nav-bar' });
   const brandLink = navBrand.querySelector('.button');
   if (brandLink) {
     brandLink.className = '';
@@ -132,7 +197,8 @@ export default async function decorate(block) {
 
   // generate links
   const navSections = nav.querySelector('.nav-sections');
-  if (navSections && isDesktop.matches) {
+  navBar.append(navSections);
+  if (navSections) {
     navSections.querySelectorAll(':scope ul > li').forEach((navSection) => {
       navSection.querySelector('a').setAttribute('class', 'text');
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
@@ -143,29 +209,22 @@ export default async function decorate(block) {
         navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
       });
     });
+    addSearchBar(navSections);
   }
-
-  // create explore link
+  const navExplore = nav.querySelector('.nav-explore');
+  navBar.append(navExplore);
   const navTools = nav.querySelector('.nav-tools');
-  if (navTools) {
-    const exploreSection = navTools.querySelector('p');
-    exploreSection.removeAttribute('class');
-    exploreSection.querySelector('a').setAttribute('class', 'text');
-  }
-
+  navBar.append(navTools);
   // hamburger for mobile
-  const hamburger = document.createElement('div');
-  hamburger.classList.add('nav-hamburger');
-  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-      <span class="nav-hamburger-icon"></span>
-    </button>`;
-  hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-  nav.append(hamburger);
   nav.setAttribute('aria-expanded', 'false');
   // prevent mobile nav behavior on window resize
-  toggleMenu(nav, navSections, isDesktop.matches);
-  isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
-
+  // toggleMenu(nav, navSections, isDesktop.matches);
+  // isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
+  nav.append(navBar);
+  // nav.append(separator);
+  // nav.append(closeButton);
+  // decorateIcons(navTools.querySelector('.burger-menu'));
+  // decorateIcons(nav.querySelector('.close-menu'));
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
