3c31f2b1f3a02e3aad057b6aabc3785869164a39
Satya Deep Maheshwari <satyam@adobe.com>
2024-02-01 18:21:37 +0530

    Further stying updates

--- blocks/link-group/link-group.js
+++ blocks/link-group/link-group.js
@@ -1,10 +1,5 @@
 export default async function decorate(block) {
   block.addEventListener('click', () => {
     block.classList.toggle('active');
-    block.parentNode.parentNode.querySelectorAll('.link-group').forEach((linkGroup) => {
-      if (linkGroup !== block) {
-        linkGroup.classList.remove('active');
-      }
-    });
   });
 }
