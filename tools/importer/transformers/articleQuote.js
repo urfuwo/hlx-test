8451129e9ad6cef89b52e78d780c9c2f41c141a1
Patrick Hupe <hupe@adobe.com>
2024-02-15 08:41:39 +0100

    import for quote (https://news.sap.com/2023/02/international-womens-day-2023-empathy-is-our-superpower-to-embrace-equity/)

--- tools/importer/transformers/articleQuote.js
+++ tools/importer/transformers/articleQuote.js
@@ -13,5 +13,21 @@ const transformQuote = (main, document) => {
     const table = WebImporter.DOMUtils.createTable(block, document);
     quote.replaceWith(table);
   });
+
+  main.querySelectorAll('p.lead:has(+ p em)').forEach((quoteTextElem) => {
+    /* quoteText: wrap entire quote text with paragraphs, remove newlines */
+    let qText = `<p>${quoteTextElem.innerHTML}</p>`;
+    qText = qText.replace(/[\n\t]/gm, '');
+    /* quoteText: replace all brs with closing / opening paragraphs */
+    qText = qText.replaceAll(/<br>/gm, '</p><p>');
+    const quoteSourceElem = quoteTextElem.nextElementSibling.firstElementChild;
+    /* quoteSource: remove newlines and replace brs with spaces */
+    let qSource = quoteSourceElem.innerHTML.replace(/[\n\t]/gm, '');
+    qSource = qSource.replaceAll(/<br>/g, ' ');
+    const block = [['Quote'], [qText], [qSource]];
+    const table = WebImporter.DOMUtils.createTable(block, document);
+    quoteTextElem.replaceWith(table);
+    quoteSourceElem.remove();
+  });
 };
 export default transformQuote;
