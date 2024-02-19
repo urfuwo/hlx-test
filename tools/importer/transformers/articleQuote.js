81cc35dd6c18b45c1f8eb9d31b0251404688f38f
Patrick Hupe <hupe@adobe.com>
2024-02-19 12:45:38 +0100

    fix: import and clean three types of quotes: - type 1: https://news.sap.com/2024/01/sap-announces-q4-and-fy-2023-results/ - type 2: https://news.sap.com/2024/01/modernize-sap-bw-with-sap-datasphere-open-data-ecosystem/ - type 3: https://news.sap.com/2023/02/international-womens-day-2023-empathy-is-our-superpower-to-embrace-equity/ fix: support multiple quotes in one blockquote element (see https://news.sap.com/2024/01/sap-announces-q4-and-fy-2023-results/)

--- tools/importer/transformers/articleQuote.js
+++ tools/importer/transformers/articleQuote.js
@@ -1,33 +1,89 @@
 /* global WebImporter */
-const transformQuote = (main, document) => {
-  main.querySelectorAll('blockquote').forEach((quote) => {
-    let quoteAttribution = '';
-    if (quote.querySelector('cite')) {
-      quoteAttribution = quote.querySelector('cite');
-      quote.querySelector('cite').remove();
-    }
-    const block = [['Quote'], [quote.innerHTML]];
-    if (quoteAttribution) {
-      block.push([quoteAttribution]);
-    }
-    const table = WebImporter.DOMUtils.createTable(block, document);
-    quote.replaceWith(table);
-  });
+function cleanQuoteText(qText) {
+  /* remove normal quotes surrounding the text */
+  let qT = qText.replace(/^"(.*)"$/gm, '$1');
+  /* remove fancy quotes surrounding the text */
+  qT = qT.replace(/^“(.*)”$/gm, '$1');
+  /* wrap entire quote text with paragraph */
+  qT = `<p>${qT}</p>`;
+  /* remove newlines */
+  qT = qT.replace(/[\n\t]/gm, '');
+  /* replace all brs with closing / opening paragraphs */
+  qT = qT.replaceAll(/<br>/gm, '</p><p>');
+  return qT;
+}
 
-  main.querySelectorAll('p.lead:has(+ p em)').forEach((quoteTextElem) => {
-    /* quoteText: wrap entire quote text with paragraphs, remove newlines */
-    let qText = `<p>${quoteTextElem.innerHTML}</p>`;
-    qText = qText.replace(/[\n\t]/gm, '');
-    /* quoteText: replace all brs with closing / opening paragraphs */
-    qText = qText.replaceAll(/<br>/gm, '</p><p>');
+function cleanQuoteSource(qSource) {
+  /* remove newlines, replace brs with whitespace */
+  let qS = qSource.replace(/[\n\t]/gm, '').replaceAll(/<br>/g, ' ');
+  /* remove <cite> elements */
+  qS = qS.replace(/<cite>(.*)<\/cite>/gm, '$1');
+  return qS;
+}
+
+/* Quote type 1: https://news.sap.com/2024/01/sap-announces-q4-and-fy-2023-results/, multiple quotes in one blockquote */
+function handleQuoteType1(main, document) {
+  const surroundingBQs = [];
+  main.querySelectorAll('blockquote p:has(+ p strong)').forEach((quoteTextElem) => {
+    const surroundingBQ = quoteTextElem.parentNode;
+    const qText = cleanQuoteText(quoteTextElem.innerHTML);
     const quoteSourceElem = quoteTextElem.nextElementSibling.firstElementChild;
-    /* quoteSource: remove newlines and replace brs with spaces */
-    let qSource = quoteSourceElem.innerHTML.replace(/[\n\t]/gm, '');
-    qSource = qSource.replaceAll(/<br>/g, ' ');
-    const block = [['Quote'], [qText], [qSource]];
+    const qSource = quoteSourceElem ? cleanQuoteSource(quoteSourceElem.innerHTML) : '';
+    const table = WebImporter.DOMUtils.createTable([['Quote'], [qText], [qSource]], document);
+    /* move newly created table before the blockquote (to preserve the order of multiple quotes) */
+    surroundingBQ.before(table);
+    /* add surroundingBQ to clean-up list */
+    if (!surroundingBQs.includes(surroundingBQ)) {
+      surroundingBQs.push(surroundingBQ);
+    }
+  });
+  /* finally: remove all remaining blockquotes */
+  surroundingBQs.forEach((surroundingBQ) => {
+    surroundingBQ.remove();
+  });
+}
+
+/* Quote type 2: https://news.sap.com/2024/01/modernize-sap-bw-with-sap-datasphere-open-data-ecosystem/ */
+function handleQuoteType2(main, document) {
+  const surroundingBQs = [];
+  main.querySelectorAll('blockquote p').forEach((quoteTextElem) => {
+    const surroundingBQ = quoteTextElem.parentNode;
+    const quoteText = cleanQuoteText(quoteTextElem.innerHTML);
+    const block = [['Quote'], [quoteText]];
+    const quoteSourceElem = surroundingBQ.querySelector('cite');
+    if (quoteSourceElem) {
+      const quoteSource = cleanQuoteSource(quoteSourceElem.innerHTML);
+      block.push([quoteSource]);
+    }
     const table = WebImporter.DOMUtils.createTable(block, document);
+    /* move newly created table before the blockquote (to preserve the order of multiple quotes) */
+    surroundingBQ.before(table);
+    /* add surroundingBQ to clean-up list */
+    if (!surroundingBQs.includes(surroundingBQ)) {
+      surroundingBQs.push(surroundingBQ);
+    }
+  });
+  /* finally: remove all surrounding blockquotes */
+  surroundingBQs.forEach((surroundingBQ) => {
+    surroundingBQ.remove();
+  });
+}
+
+/* Quote type 3: https://news.sap.com/2023/02/international-womens-day-2023-empathy-is-our-superpower-to-embrace-equity/ */
+function handleQuoteType3(main, document) {
+  main.querySelectorAll('p.lead:has(+ p em)').forEach((quoteTextElem) => {
+    const qText = cleanQuoteText(quoteTextElem.innerHTML);
+    const quoteSourceElem = quoteTextElem.nextElementSibling.firstElementChild;
+    const qSource = cleanQuoteSource(quoteSourceElem.innerHTML);
+    const table = WebImporter.DOMUtils.createTable([['Quote'], [qText], [qSource]], document);
     quoteTextElem.replaceWith(table);
     quoteSourceElem.remove();
   });
+}
+
+const transformQuote = (main, document) => {
+  handleQuoteType1(main, document);
+  handleQuoteType2(main, document);
+  handleQuoteType3(main, document);
 };
 export default transformQuote;
