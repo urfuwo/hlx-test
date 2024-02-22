e4101d081cdca15e39bf5a75c39f3a821a678e59
Patrick Hupe <hupe@adobe.com>
2024-02-22 13:44:56 +0100

    fix: removed quotemark divs

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,17 +1,8 @@
-import {
-  div,
-} from '../../scripts/dom-builder.js';
-
 export default function decorate(block) {
   const qtContent = block.querySelector(':scope > div:first-of-type > div');
   qtContent.classList.add('col', 'content');
-
+  qtContent.parentNode.classList.add('qt');
   const qsContent = block.querySelector(':scope > div:nth-of-type(2) > div');
   qsContent.classList.add('col', 'content');
-
-  qtContent.parentNode.classList.add('qt');
-  qtContent.before(div({ class: 'col qmcol oqm' }));
-  qtContent.after(div({ class: 'col qmcol cqm' }));
-
   qsContent.parentNode.classList.add('qs');
 }
