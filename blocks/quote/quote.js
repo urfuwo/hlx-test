dd9a6f7e6649f55baa454562bd61492208fe5f83
Patrick Hupe <hupe@adobe.com>
2024-02-22 11:35:39 +0100

    fix: minimized JS DOM manipulation fix: removed placeholder divs

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,31 +3,15 @@ import {
 } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-  const quoteTextBlock = block.querySelector(':scope > div:first-of-type');
-  const quoteSourceBlock = block.querySelector(':scope > div:nth-of-type(2)');
-  block.innerHTML = '';
+  const qtContent = block.querySelector(':scope > div:first-of-type > div');
+  qtContent.classList.add('col', 'content');
 
-  const quoteTextContainer = div({ class: 'col content' });
-  quoteTextContainer.append(...quoteTextBlock.childNodes);
+  const qsContent = block.querySelector(':scope > div:nth-of-type(2) > div');
+  qsContent.classList.add('col', 'content');
 
-  const quoteSourceContainer = div({ class: 'col content' });
-  quoteSourceContainer.append(...quoteSourceBlock.childNodes);
+  qtContent.parentNode.classList.add('qt');
+  qtContent.before(div({ class: 'col qmcol oqm' }));
+  qtContent.after(div({ class: 'col qmcol cqm' }));
 
-  // quoteText row
-  const qtRow = div(
-    { class: 'row' },
-    div({ class: 'col qmcol oqm' }),
-    quoteTextContainer,
-    div({ class: 'col qmcol cqm' }),
-  );
-  block.append(qtRow);
-
-  // quoteSource row
-  const qsRow = div(
-    { class: 'row' },
-    div({ class: 'col qmcol' }),
-    quoteSourceContainer,
-    div({ class: 'col qmcol' }),
-  );
-  block.append(qsRow);
+  qsContent.parentNode.classList.add('qs');
 }
