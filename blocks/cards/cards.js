7d39124d887864bd653056ba65a44500612ef528
Ben Peter <benp@adobe.com>
2024-02-23 01:04:57 +0100

    product cards for alignment on #120

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -8,11 +8,18 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else {
+        div.className = 'cards-card-body';
+        const h3 = div.querySelector('h4');
+        if (h3.innerText.startsWith('| ')) {
+          h3.innerText = h3.innerText.substring(2);
+          h3.classList.add('sapproduct');
+        }
+      }
     });
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '100' }])));
   block.textContent = '';
   block.append(ul);
 }
