92c6f11e48bf7e3ad4763c7c4d8ed57203c41680
Markus Haack <mhaack@adobe.com>
2024-02-23 11:03:39 +0100

    Chore: add support for video placeholder and more panels

--- tools/importer/transformers/articlePanel.js
+++ tools/importer/transformers/articlePanel.js
@@ -3,26 +3,35 @@ const articlePanel = (main, document) => {
   document.querySelectorAll('.entry-content .panel').forEach((panel) => {
     // detecting event panels
     // sample: https://news.sap.com/2023/02/international-womens-day-2023-empathy-is-our-superpower-to-embrace-equity/
+    const cells = [];
     const content = panel.querySelector('p');
     const registerLinks = panel.querySelector('ul');
-    const buttons = document.createElement('div');
-    registerLinks?.querySelectorAll('li').forEach((li) => {
-      if (li.querySelector('a > strong')) {
-        const wrapper = document.createElement('p');
-        const link = li.querySelector('a');
-        wrapper.append(link);
-        [...link.childNodes].forEach((n) => {
-          if (n.nodeName !== 'STRONG') {
-            wrapper.append(n);
-          }
-        });
-        buttons.append(wrapper);
-      } else {
-        buttons.append(li.innerHTML);
+    if (registerLinks) {
+      cells.push(['', content]);
+      const buttons = document.createElement('div');
+      registerLinks?.querySelectorAll('li').forEach((li) => {
+        if (li.querySelector('a > strong')) {
+          const wrapper = document.createElement('p');
+          const link = li.querySelector('a');
+          wrapper.append(link);
+          [...link.childNodes].forEach((n) => {
+            if (n.nodeName !== 'STRONG') {
+              wrapper.append(n);
+            }
+          });
+          buttons.append(wrapper);
+        } else {
+          buttons.append(li.innerHTML);
+        }
+      });
+      if (buttons.childElementCount > 0) {
+        cells.push(buttons);
       }
-    });
+    } else {
+      cells.push(content);
+    }
 
-    const block = [['Promo'], ['', content, buttons]];
+    const block = [['Promo'], cells];
     const table = WebImporter.DOMUtils.createTable(block, document);
     panel.replaceWith(table);
   });
--- tools/importer/transformers/youtubeVideo.js
+++ tools/importer/transformers/youtubeVideo.js
@@ -1,11 +1,21 @@
+/* global WebImporter */
 export default function transformVideo(main, document, html) {
-  const videos = document.querySelectorAll('div.flex-video, figure.is-provider-youtube');
+  const videos = document.querySelectorAll('div.flex-video, figure.is-provider-youtube, section[data-inpsyde-embed="youtube"]');
   videos.forEach((video) => {
     const link = video.querySelector('a[rel=noreferrer]');
     if ((link && link.href.indexOf('youtube.com') !== -1) || link.href.indexOf('youtu.be') !== -1) {
       const videolink = document.createElement('p');
       videolink.textContent = link.href;
-      video.replaceWith(videolink);
+      const placeholder = video.querySelector('div[data-inpsyde-embed-preview="youtube"] > img');
+      if (placeholder) {
+        const wrapper = document.createElement('div');
+        wrapper.append(placeholder, videolink);
+        const block = [['Embed'], [wrapper]];
+        const table = WebImporter.DOMUtils.createTable(block, document);
+        video.replaceWith(table);
+      } else {
+        video.replaceWith(videolink);
+      }
     }
   });
 }
