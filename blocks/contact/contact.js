9a1b9e610f643d534337f056b8b22ca15906034f
Markus Haack <mhaack@adobe.com>
2024-02-29 15:52:44 +0100

    fix: contact

--- blocks/contact/contact.js
+++ blocks/contact/contact.js
@@ -1,5 +1,5 @@
 import {
-  img, select, option, div, label,
+  img, select, option, div,
 } from '../../scripts/dom-builder.js';
 
 export default async function decorate(block) {
@@ -11,9 +11,8 @@ export default async function decorate(block) {
         class: ['icon', 'icon-sap'],
         alt: 'SAP',
       }),
-      label({ for: 'lang-picker' }, 'Select Language: '),
       select(
-        { id: 'lang-picker' },
+        { 'aria-label': 'Country Selector' },
         option({ value: 'us' }, 'United States - English'),
         option({ value: 'cn' }, '中文 - Chinese'),
       ),
