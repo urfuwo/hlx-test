1bd7f0d6b739a8f97eb26f73c922a5104c15e87b
Patrick Hupe <hupe@adobe.com>
2024-03-05 10:00:46 +0100

    fix: editors can specify author by URL or by name fix: horizontal and vertical author profiles supported fix: lists of 2 and 3 enforce a grid/row of 2 (or 3, respectively) fix: vertical and list-of-1 show using vertical orientation chore: refactoring of author-profile.js to reuse in author-profiles.js

--- blocks/author-profile/author-profile.js
+++ blocks/author-profile/author-profile.js
@@ -11,13 +11,13 @@ const breakpoints = [
   { width: '120' },
 ];
 
-function extractAuthorDescription(details) {
-  let { description } = details;
+function extractAuthorDescription(entry) {
+  let { description } = entry;
   if (!description) {
-    if (details.title) {
-      description = details.title;
-      if (details.author && details.title.startsWith(`${details.author}, `)) {
-        description = details.title.substring(details.author.length + 2);
+    if (entry.title) {
+      description = entry.title;
+      if (entry.author && entry.title.startsWith(`${entry.author}, `)) {
+        description = entry.title.substring(entry.author.length + 2);
       }
     } else {
       description = 'Author';
@@ -26,34 +26,49 @@ function extractAuthorDescription(details) {
   return description;
 }
 
-async function getAuthorDetails() {
-  // #todo: handle multiple authors
-  const name = getMetadata('author');
-  const result = name
-    ? await ffetch('/authors-index.json').filter((entry) => entry.author === name).limit(1).all() : null;
-  const details = (!result || result.length < 1)
-    ? { path: '/author/name', author: 'Name' } : result[0];
-  details.description = extractAuthorDescription(details);
-  return details;
+function completeEntry(entry) {
+  const result = (!entry) ? { path: '/author/name', author: 'Name' } : entry;
+  result.description = extractAuthorDescription(result);
+  return result;
 }
 
-export default async function decorate(block) {
-  const details = await getAuthorDetails();
-  const authorImage = details.image
-    ? createOptimizedPicture(details.image, details.author, false, breakpoints) : null;
+async function getAuthorEntry(entryFilter) {
+  const result = await ffetch('/authors-index.json').filter(entryFilter).limit(1).all();
+  return completeEntry((!result || result.length < 1) ? null : result[0]);
+}
+
+function renderProfile(entry) {
+  const authorImage = entry.image
+    ? createOptimizedPicture(entry.image, entry.author, false, breakpoints) : null;
   const authorProfile = div(
     div({ class: 'avatar' }, authorImage ? div(authorImage) : div()),
     div(
       { class: 'details' },
-      h2(details.author),
-      p(details.description),
+      h2(entry.author),
+      p(entry.description),
       p(
         { class: 'link' },
-        a({ href: details.path, 'aria-label': 'Read more' }, 'See more by this author'),
+        a({ href: entry.path, 'aria-label': 'Read more' }, 'See more by this author'),
         span({ class: 'icon icon-link-arrow' }),
       ),
     ),
   );
   decorateIcons(authorProfile);
+  return authorProfile;
+}
+
+export default async function decorate(block) {
+  // #todo: handle multiple authors
+  const name = getMetadata('author');
+  const entryFilter = ((entry) => entry.author === name);
+  const entry = await getAuthorEntry(entryFilter);
+  const authorProfile = await renderProfile(entry);
+  block.classList.add('ver');
   block.append(authorProfile);
 }
+
+export {
+  decorate,
+  renderProfile,
+  completeEntry,
+};
---
+++ blocks/author-profiles/author-profiles.js
@@ -0,0 +1,43 @@
+import { loadCSS } from '../../scripts/aem.js';
+import ffetch from '../../scripts/ffetch.js';
+import { div } from '../../scripts/dom-builder.js';
+import { completeEntry, renderProfile } from '../author-profile/author-profile.js';
+
+async function getAuthorEntries(keys) {
+  const entryFilter = ((entry) => (keys.includes(entry.author) || keys.includes(entry.path)));
+  const unsortedEntries = await ffetch('/authors-index.json').filter(entryFilter).limit(keys.length).all();
+  const sortedEntries = [];
+  keys.forEach((key) => {
+    // eslint-disable-next-line max-len
+    sortedEntries.push(completeEntry(unsortedEntries.find((entry) => (key === entry.author || key === entry.path))));
+  });
+  return sortedEntries;
+}
+
+async function addAuthorProfiles(block, keys) {
+  const entries = await getAuthorEntries(keys);
+  if (keys.length > 1) {
+    block.classList.add(`elems${keys.length}`);
+    entries.forEach((entry) => {
+      const profile = div({ class: 'author-profile hor' }, renderProfile(entry));
+      block.append(profile);
+    });
+  } else {
+    block.classList.add('vertical');
+    block.append(div({ class: 'author-profile' }, renderProfile(entries[0])));
+  }
+}
+
+export default async function decorateBlock(block) {
+  loadCSS(`${window.hlx.codeBasePath}/blocks/author-profile/author-profile.css`);
+  const keys = [];
+  block.querySelectorAll(':scope p').forEach((para) => {
+    const link = para.querySelector('a');
+    keys.push(link ? (new URL(link.href).pathname) : para.innerText);
+  });
+  if (keys.length === 0) {
+    keys.push(block.querySelector(':scope div div').innerText);
+  }
+  block.innerHTML = '';
+  await addAuthorProfiles(block, keys);
+}
