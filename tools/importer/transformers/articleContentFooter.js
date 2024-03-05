2c1b59fda389931bf5104650e4d9a7981f679b9d
Markus Haack <mhaack@adobe.com>
2024-03-05 15:57:48 +0100

    fix: footnote mapping

--- tools/importer/transformers/articleContentFooter.js
+++ tools/importer/transformers/articleContentFooter.js
@@ -1,15 +1,5 @@
 /* global WebImporter */
 const articleContentFooter = (main, document) => {
-  const footnoteParagraph = document.querySelector(
-    'section#main > article div.entry-content p.has-small-font-size:last-child, section#main > article div.entry-content p > small',
-  );
-  if (footnoteParagraph) {
-    footnoteParagraph.querySelectorAll('br').forEach((br) => br.remove());
-    const block = [['Article Footnote'], [footnoteParagraph.outerHTML]];
-    const table = WebImporter.DOMUtils.createTable(block, document);
-    footnoteParagraph.replaceWith(table);
-  }
-
   const footnoteWrapper = document.querySelector(
     'section#main > article div.entry-content div.footnote-wrapper',
   );
