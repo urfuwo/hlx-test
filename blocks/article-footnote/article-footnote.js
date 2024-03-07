4524aa5c6bb8f0fad57176bce40f23d905c8c7d3
Patrick Hupe <hupe@adobe.com>
2024-03-07 14:22:59 +0100

    fix: review comments

--- blocks/article-footnote/article-footnote.js
+++ blocks/article-footnote/article-footnote.js
@@ -1,12 +1,11 @@
-import { div, p } from '../../scripts/dom-builder.js';
+import { div } from '../../scripts/dom-builder.js';
 import { getMetadata } from '../../scripts/aem.js';
 import { formatDate } from '../../scripts/utils.js';
 
 export default async function decorateBlock(block) {
-  const content = block.firstElementChild.firstElementChild;
   const meta = [
     getMetadata('published-time') ? `Published on ${formatDate(getMetadata('published-time'))}` : null,
     getMetadata('modified-time') ? `Updated on ${formatDate(getMetadata('modified-time'))}` : null,
   ];
-  content.replaceWith(div(p(content.textContent), p(meta.filter(Boolean).join('. '))));
+  block.firstElementChild.append(div(meta.filter(Boolean).join('. ')));
 }
