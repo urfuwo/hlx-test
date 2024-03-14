1e8cca79a16cfcf078aa077f3d85d353ce7a7eb7
Saurabh Khare <skhare@adobe.com>
2024-03-14 14:34:45 +0100

    Removed older path for template

--- templates/hub-l1/hub-l1.js
+++
@@ -1,8 +0,0 @@
-import { containerize } from '../../scripts/utils.js';
-
-function decorate(doc) {
-  const main = doc.querySelector('main');
-  containerize(main, '.hero');
-}
-
-decorate(document);
