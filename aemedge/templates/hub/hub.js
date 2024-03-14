2e19c9e52513ed15afe31de3c488fbc1f2cae670
Saurabh Khare <skhare@adobe.com>
2024-03-14 15:56:36 +0100

    removed L1 specific template

--- aemedge/templates/hub/hub.js
+++ aemedge/templates/hub/hub.js
@@ -1,10 +1,12 @@
 import { containerize } from '../../scripts/utils.js';
 import { aside } from '../../scripts/dom-builder.js';
+import { getMetadata } from '../../scripts/aem.js';
 
 function decorate(doc) {
   const main = doc.querySelector('main');
   containerize(main, '.hero');
-  main.parentNode.insertBefore(aside(), main);
+  const template = getMetadata('template');
+  if (template === 'hub-l2') main.parentNode.insertBefore(aside(), main);
 }
 
 decorate(document);
