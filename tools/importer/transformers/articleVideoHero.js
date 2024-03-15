c86508589e9f492a76089435f4355bee2e40a6c7
Markus Haack <mhaack@adobe.com>
2024-03-15 09:38:22 +0100

    chore: fix video heros

--- tools/importer/transformers/articleVideoHero.js
+++ tools/importer/transformers/articleVideoHero.js
@@ -2,9 +2,15 @@
 const transformVideoHero = (main, document) => {
   const heading = document.querySelector('section#main > article.sap-tv h1');
   if (heading) {
-    //
     const wrapper = document.createElement('div');
-    const eyebrow = document.createElement('p');
+
+    // get hero image from video thumbnail
+    const videoThumbnail = document.querySelector('section#main > article.sap-tv > div.flex-video img');
+    if (videoThumbnail) {
+      wrapper.append(videoThumbnail.cloneNode(true));
+    }
+
+    const eyebrow = document.createElement('h6');
     eyebrow.textContent = 'Video';
     wrapper.append(eyebrow, heading);
 
