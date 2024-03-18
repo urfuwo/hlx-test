8747c04d40cafc5eb59f36a0f429ffde6fa200ff
Patrick Hupe <hupe@adobe.com>
2024-03-18 18:28:16 +0100

    chore: fix linter errors

--- aemedge/libs/pictureCard/pictureCard.js
+++ aemedge/libs/pictureCard/pictureCard.js
@@ -3,7 +3,7 @@ import {
   li, a, span, div, p,
 } from '../../scripts/dom-builder.js';
 import { createOptimizedPicture, toClassName, loadCSS } from '../../scripts/aem.js';
-import { renderProfile } from '../profile/profile.js';
+import { renderProfile } from '../../scripts/profile.js';
 
 export default class PictureCard extends Card {
   constructor(title, path, type, info, authorEntry, image, tagLabel, description) {
