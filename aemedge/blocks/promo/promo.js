0d6bf618bf031fc39c5da2ea5aa7a9ae7a9bc4d7
sanmishr <sanmishr@adobe.com>
2024-03-19 09:23:30 +0100

    fixed 237 and 238 styling issues

--- aemedge/blocks/promo/promo.js
+++ aemedge/blocks/promo/promo.js
@@ -1,16 +1,22 @@
+import { loadCSS } from '../../scripts/aem.js';
+
 export default async function decorate(block) {
-  const blockChildrens = block.getElementsByTagName('div');
-  const buttonElem = blockChildrens.item(3);
-  const buttonContainer = buttonElem.getElementsByTagName('p');
-  Array.from(buttonContainer).forEach((elem) => {
-    elem.classList.add(...['button-container', 'no-margin']);
-    const buttonType = elem.firstElementChild;
-    const btnClasses = ['button'];
-    if (buttonType.tagName === 'EM') {
-      btnClasses.push('secondary');
-    } else {
-      btnClasses.push('primary');
+  loadCSS(`${window.hlx.codeBasePath}/libs/button/button.css`);
+
+  const blockChildren = block.querySelectorAll('div + div p');
+  const arrElemsWithButtons = Array.from(blockChildren).filter((childElem) => Boolean(childElem.querySelector('a')));
+  arrElemsWithButtons.forEach((elem) => {
+    const hasButton = elem.querySelector('a');
+    if (hasButton) {
+      elem.classList.remove('button-wrapper');
+      elem.classList.add('button-container');
+      const btnClasses = ['button', 'medium', 'no-margin'];
+      elem.firstChild?.firstChild?.classList?.add(...btnClasses);
+      if (elem.firstChild.tagName === 'EM') {
+        elem.firstChild?.firstChild?.classList?.add('secondary');
+      } else {
+        elem.firstChild?.firstChild?.classList?.add('primary');
+      }
     }
-    buttonType.firstElementChild.classList.add(...btnClasses);
   });
 }
