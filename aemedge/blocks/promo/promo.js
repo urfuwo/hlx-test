48189d2bb4132df05ecf6343d3ee821b3b888056
Markus Haack <mhaack@adobe.com>
2024-03-19 13:59:31 +0100

    chore: update styles

--- aemedge/blocks/promo/promo.js
+++ aemedge/blocks/promo/promo.js
@@ -1,23 +1,5 @@
+import { decorateButtons } from '../../scripts/aem.js';
+
 export default async function decorate(block) {
-  const blockChildren = block.querySelectorAll('div + div p');
-  const arrElemsWithButtons = Array.from(blockChildren).filter((childElem) => Boolean(childElem.querySelector('a')));
-  arrElemsWithButtons.forEach((elem) => {
-    const hasButton = elem.querySelector('a');
-    if (hasButton) {
-      elem.classList.remove('button-wrapper');
-      elem.classList.add('button-container');
-      const btnClasses = ['button', 'medium', 'no-margin'];
-      if (elem.firstChild?.tagName === 'A') {
-        const anchorButtonClasses = [...btnClasses, 'primary'];
-        elem.firstChild?.classList?.add(...anchorButtonClasses);
-      } else {
-        elem.firstChild?.firstChild?.classList?.add(...btnClasses);
-        if (elem.firstChild.tagName === 'EM') {
-          elem.firstChild?.firstChild?.classList?.add('secondary');
-        } else {
-          elem.firstChild?.firstChild?.classList?.add('primary');
-        }
-      }
-    }
-  });
+  decorateButtons(block);
 }
