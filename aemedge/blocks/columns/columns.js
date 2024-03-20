24c2e2c4fa20e360c68bc0aabf3f5e66b88fa8a5
Peter Antal <52969899+rootpea@users.noreply.github.com>
2024-03-20 20:22:01 +0100

    267-allow fragments in columns block

--- aemedge/blocks/columns/columns.js
+++ aemedge/blocks/columns/columns.js
@@ -1,4 +1,92 @@
-export default function decorate(block) {
+import {
+  decorateMain,
+} from '../../scripts/scripts.js';
+
+import {
+  loadBlocks,
+} from '../../scripts/aem.js';
+
+/**
+ * Loads a fragment.
+ * @param {string} path The path to the fragment
+ * @returns {HTMLElement} The root element of the fragment
+ */
+export async function loadFragment(path) {
+  if (path && path.startsWith('/') && path.includes('/fragments/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+
+      // reset base path for media to fragment base
+      const resetAttributeBase = (tag, attr) => {
+        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+        });
+      };
+      resetAttributeBase('img', 'src');
+      resetAttributeBase('source', 'srcset');
+
+      await decorateMain(main, false);
+      await loadBlocks(main);
+      return main;
+    }
+  }
+  return null;
+}
+
+function findAndMarkFragmentLinks(block) {
+  const links = block.querySelectorAll('a');
+  [...links].forEach((link) => {
+    const path = link.getAttribute('href');
+    if (path && path.startsWith('/') && path.includes('/fragments/')) {
+      link.classList.add('fragment-link');
+    }
+  });
+}
+
+async function replaceLinkWithFragment(block, col, link) {
+  const path = link.getAttribute('href');
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      block.closest('.section').classList.add(...fragmentSection.classList);
+      let linkParent = link.parentNode;
+      if (linkParent.childNodes.length === 1) {
+        while (linkParent.parentNode !== col && linkParent.childNodes.length === 1) {
+          linkParent = linkParent.parent;
+        }
+        linkParent.replaceWith(...fragment.childNodes);
+      } else {
+        link.replaceWith(...fragment.childNodes);
+      }
+    }
+  }
+}
+
+function removeButtonContainerClassWithNoLinks(block) {
+  const buttonContainers = block.querySelectorAll('.button-container');
+  [...buttonContainers].forEach((bc) => {
+    const links = bc.querySelectorAll('a:not(.fragment-link)');
+    if (links.length === 0) {
+      bc.classList.remove('button-container');
+    }
+  });
+}
+
+function convertFragmentLinksToFragments(block) {
+  [...block.children].forEach(async (row) => {
+    [...row.children].forEach(async (col) => {
+      const links = col.querySelectorAll('a.fragment-link');
+      [...links].forEach(async (link) => {
+        replaceLinkWithFragment(block, col, link);
+      });
+    });
+  });
+}
+
+export default async function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
@@ -15,4 +103,10 @@ export default function decorate(block) {
       }
     });
   });
+
+  findAndMarkFragmentLinks(block);
+
+  removeButtonContainerClassWithNoLinks(block);
+
+  convertFragmentLinksToFragments(block);
 }
