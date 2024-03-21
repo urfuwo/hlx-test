ec0d8f22956572e64fb7748902c03fca8ae58da5
sanmishr <sanmishr@adobe.com>
2024-03-21 10:25:16 +0100

    icon link issue fixed

--- aemedge/blocks/promo/promo.js
+++ aemedge/blocks/promo/promo.js
@@ -1,5 +1,17 @@
-import { decorateButtons } from '../../scripts/aem.js';
+import { decorateButtons, decorateIcons } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   decorateButtons(block);
+
+  const parentElems = document.querySelectorAll('.default-content-wrapper .button-container');
+  Array.from(parentElems).forEach((elem) => {
+    elem.classList.remove('button-container');
+    elem.classList.add('link-container');
+    const iconSpan = document.createElement('span');
+    iconSpan.classList.add(...['icon', 'icon-link-arrow']);
+    elem.appendChild(iconSpan);
+    elem.firstChild.classList.remove('button');
+    elem.firstChild.classList.add('link');
+    decorateIcons(elem);
+  });
 }
