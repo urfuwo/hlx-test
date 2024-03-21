b37f6e2c9e2a118a342ef75f27c01c3e1e2bb669
Peter Antal <52969899+rootpea@users.noreply.github.com>
2024-03-21 14:59:42 +0100

    use loadfragment from fragment.js

--- aemedge/blocks/columns/columns.js
+++ aemedge/blocks/columns/columns.js
@@ -1,39 +1,6 @@
 import {
-  decorateMain,
-} from '../../scripts/scripts.js';
-
-import {
-  loadBlocks,
-} from '../../scripts/aem.js';
-
-/**
- * Loads a fragment.
- * @param {string} path The path to the fragment
- * @returns {HTMLElement} The root element of the fragment
- */
-async function loadFragment(path) {
-  if (path && path.startsWith('/') && path.includes('/fragments/')) {
-    const resp = await fetch(`${path}.plain.html`);
-    if (resp.ok) {
-      const main = document.createElement('main');
-      main.innerHTML = await resp.text();
-
-      // reset base path for media to fragment base
-      const resetAttributeBase = (tag, attr) => {
-        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-        });
-      };
-      resetAttributeBase('img', 'src');
-      resetAttributeBase('source', 'srcset');
-
-      await decorateMain(main, false);
-      await loadBlocks(main);
-      return main;
-    }
-  }
-  return null;
-}
+  loadFragment,
+} from '../fragment/fragment.js';
 
 function findAndMarkFragmentLinks(block) {
   const links = block.querySelectorAll('a');
