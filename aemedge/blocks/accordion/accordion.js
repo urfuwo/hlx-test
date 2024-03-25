1b50dbfd188485adf0b99c2cc3c723b44157edf8
Rachel Waterman <rwaterman@adobe.com>
2024-03-25 17:12:24 +0000

    337: JS refactoring and styling changes to align Accordion with Frontify designs.

---
+++ aemedge/blocks/accordion/accordion.js
@@ -0,0 +1,40 @@
+/*
+ * Accordion Block
+ * Recreate an accordion
+ * https://www.hlx.live/developer/block-collection/accordion
+ */
+
+import { domEl, p, span } from '../../scripts/dom-builder.js';
+import { decorateIcons } from '../../scripts/aem.js';
+
+function hasWrapper(el) {
+  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+}
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summaryArrow = span({ class: 'icon icon-slim-arrow-right accordion-arrow' });
+    const summaryContents = [summaryArrow, ...label.childNodes];
+    const summary = domEl(
+      'summary',
+      { class: 'accordion-item-label' },
+      ...(!hasWrapper(label) ? [p(...summaryContents)] : summaryContents),
+    );
+
+    // decorate accordion item body
+    const body = !hasWrapper(row.children[1]) ? p(...row.children[1]) : row.children[1];
+    body.className = 'accordion-item-body';
+
+    // decorate accordion item
+    const details = domEl(
+      'details',
+      { class: 'accordion-item' },
+      summary,
+      body,
+    );
+    decorateIcons(details);
+    row.replaceWith(details);
+  });
+}
