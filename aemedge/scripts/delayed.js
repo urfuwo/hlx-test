a26f88f3e2b9e40226d95a5e2a5d7946e1e7aa48
Patrick Hupe <hupe@adobe.com>
2024-04-11 19:33:11 +0200

    chore: alphabetical order of path prefixes

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -16,7 +16,7 @@ function getEnvType(hostname = window.location.hostname) {
 }
 
 function getScope(path = window.location.pathname) {
-  const fullScopePrefixes = ['/events/', '/research/', '/podcasts/', '/resources/', '/videos/'];
+  const fullScopePrefixes = ['/events/', '/podcasts/', '/research/', '/resources/', '/videos/'];
   return fullScopePrefixes.some((prefix) => path.startsWith(prefix)) ? 'full' : 'mvp';
 }
 
