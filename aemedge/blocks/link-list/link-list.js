1773ce94d08b759ddf14c72dff5b5e8902611306
Eduard Seifert <1193638+eduardseifert@users.noreply.github.com>
2024-04-12 10:39:54 +0200

    #423:
    - streamlined icon handling (now part of the link in Sharepoint)
    - improved A11Y while tabbing
    - removed unnecessary styles
    - wrapped link text node in `span` (to have a better CSS handling)

--- aemedge/blocks/link-list/link-list.js
+++ aemedge/blocks/link-list/link-list.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * Replace an `img` (icon) element with an SVG element
+ * Replace the `img` (icon) element with the referenced SVG element.
  * @param img {Element} The `img` element to replace
  */
 function replaceIconImgWithSvg(img) {
@@ -34,9 +34,31 @@ function replaceIconImgWithSvg(img) {
     });
 }
 
+/**
+ * Wrap the text node of a link with a `span` element.
+ * (!) Text node can also be positioned before the icon.
+ * @param link {Element} The link element
+ */
+function moveLinkTextNodeToSpan(link) {
+  Array.from(link.childNodes)
+    .forEach((node) => {
+      if (node.nodeType === Node.TEXT_NODE && node.textContent.trim().length > 0) {
+        const span = document.createElement('span');
+        span.classList.add('button-text');
+        const textNode = node.cloneNode(true);
+        span.appendChild(textNode);
+        link.replaceChild(span, node);
+      }
+    });
+}
+
 export default function decorate(block) {
   [...block.children].forEach((entry) => {
     const iconImg = entry.querySelector('span.icon img');
+    const link = entry.querySelector('div a.button');
+    if (link) {
+      moveLinkTextNodeToSpan(link);
+    }
     if (iconImg) {
       replaceIconImgWithSvg(iconImg);
     }
