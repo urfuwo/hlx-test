e1dec894af64c56e1dc06291880960e22ccb7333
Eduard Seifert <1193638+eduardseifert@users.noreply.github.com>
2024-04-15 12:04:18 +0200

    #423: styled the SVG icons directly on the `img` tag with CSS filter, removed the conversion from `img` to SVG from the JS block

--- aemedge/blocks/link-list/link-list.js
+++ aemedge/blocks/link-list/link-list.js
@@ -3,38 +3,6 @@
  */
 
 /**
- * Replace the `img` (icon) element with the referenced SVG element.
- * @param img {Element} The `img` element to replace
- */
-function replaceIconImgWithSvg(img) {
-  const imgSrc = img.getAttribute('src');
-
-  fetch(imgSrc)
-    .then((response) => response.text())
-    .then((data) => {
-      // Create a container for the SVG
-      const div = document.createElement('div');
-      div.innerHTML = data;
-
-      // Get the SVG element
-      const svg = div.getElementsByTagName('svg')[0];
-
-      // Get all path elements within the SVG
-      const paths = svg.querySelectorAll('path');
-      paths.forEach((path) => {
-        path.removeAttribute('fill');
-      });
-
-      // Replace the img element with the SVG
-      img.parentNode.replaceChild(svg, img);
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.error(error);
-    });
-}
-
-/**
  * Wrap the text node of a link with a `span` element.
  * (!) Text node can also be positioned before the icon.
  * @param link {Element} The link element
@@ -54,13 +22,9 @@ function moveLinkTextNodeToSpan(link) {
 
 export default function decorate(block) {
   [...block.children].forEach((entry) => {
-    const iconImg = entry.querySelector('span.icon img');
     const link = entry.querySelector('div a.button');
     if (link) {
       moveLinkTextNodeToSpan(link);
-    }
-    if (iconImg) {
-      replaceIconImgWithSvg(iconImg);
     }
   });
 }
