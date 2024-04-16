7b77064f4b254d2f1e118ab54e7f870ba2cf3d2a
Rachel Waterman <rwaterman@adobe.com>
2024-04-16 23:03:51 +0100

    355: TOC improvements - Hover styling, Nested items for H3, Styling tweaks to align with Frontify, Heading from metadata or placeholders, Restrict header query to default content blocks outside of TOC/sidebar/hero/footer.

--- aemedge/blocks/toc/toc.js
+++ aemedge/blocks/toc/toc.js
@@ -1,38 +1,100 @@
 import {
-  h2, ol, li, span, a, aside,
+  h2, ol, li, span, a, aside, div,
 } from '../../scripts/dom-builder.js';
+import { fetchPlaceholders, getMetadata, toCamelCase } from '../../scripts/aem.js';
 
 function setActiveLink() {
   const links = document.querySelectorAll('.toc li');
   links.forEach((link) => {
-    const linkHash = link.querySelector('a')?.hash;
-    if (linkHash === window.location.hash) {
+    const anchor = link.querySelector('a');
+    const linkHash = anchor?.hash;
+    const isH3 = anchor?.classList.contains('toc-h3');
+    const linkMatch = linkHash === window.location.hash;
+    if (linkMatch) {
       link.setAttribute('aria-current', 'true');
+      if (isH3) {
+        link.closest('.toc__list-item.parent').classList.add('expanded');
+      } else if (link.classList.contains('parent')) {
+        link.classList.add('expanded');
+      }
     } else {
       link.setAttribute('aria-current', 'false');
+      link.classList.remove('expanded');
     }
   });
 }
 
+function buildListItemLink(header) {
+  return a({
+    href: `#${header.id}`,
+    class: `toc-${header.tagName.toLowerCase()}`,
+  }, header.innerText);
+}
+
+function buildListItemContent(header) {
+  return div(
+    { class: 'toc__list-item__content' },
+    span(),
+    buildListItemLink(header),
+  );
+}
+
+function buildListItem(header, subheaders) {
+  const hasSubheaders = subheaders && subheaders.length > 0;
+  return li(
+    { class: `toc__list-item ${hasSubheaders ? 'parent' : ''}` },
+    buildListItemContent(header),
+    hasSubheaders ? ol(
+      { class: 'toc__list' },
+      ...subheaders.map(
+        (subheader) => li(
+          { class: 'toc__list-item' },
+          buildListItemContent(subheader),
+        ),
+      ),
+    ) : '',
+  );
+}
+
+function buildList(headers) {
+  const tocList = ol({ class: 'toc__list' });
+  let currentParentHeader;
+  let subheaders = [];
+  headers.forEach((header, index) => {
+    const isH2 = header.tagName === 'H2';
+    if (isH2 && currentParentHeader) {
+      tocList.appendChild(buildListItem(currentParentHeader, subheaders));
+      currentParentHeader = header;
+      subheaders = [];
+    } else if (isH2) {
+      currentParentHeader = header;
+    } else {
+      subheaders.push(header);
+    }
+    if (index === headers.length - 1) {
+      tocList.appendChild(buildListItem(currentParentHeader, subheaders));
+    }
+  });
+  return tocList;
+}
+
 export default async function decorate(block) {
-  const mainContent = document.querySelector('main > :nth-child(3)');
-  const headers = mainContent?.querySelectorAll('h2, h3');
+  const mainContent = document.querySelectorAll(
+    'main > .section:not(.hero-container, .toc-container, .section[data-location="sidebar"], .section[data-location="document-footer"]) .default-content-wrapper',
+  );
+  const headers = mainContent ? Array.from(mainContent).reduce((acc, currentSection) => {
+    acc.push(...currentSection.querySelectorAll('h2, h3'));
+    return acc;
+  }, []) : [];
   if (headers.length > 0) {
-    const tocElement = aside({ class: 'toc' }, h2('What\'s on this page'));
-    const tocList = ol();
-    headers.forEach((header) => {
-      const entry = li(
-        span(),
-        a({
-          href: `#${header.id}`,
-          class: `toc-${header.tagName.toLowerCase()}`,
-        }, header.innerText),
-      );
-      tocList.appendChild(entry);
-    });
-    tocElement.appendChild(tocList);
+    const placeholders = await fetchPlaceholders();
+    const tocElement = aside(
+      { class: 'toc' },
+      h2(getMetadata('toc-heading') || placeholders[toCamelCase('toc-heading')]),
+      buildList(headers),
+    );
     block.append(tocElement);
-    window.addEventListener('load', setActiveLink);
+    setActiveLink();
     window.addEventListener('hashchange', setActiveLink);
   }
 }
