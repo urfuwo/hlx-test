caea8be344e7b00ee3ee5f56aa99c974d5434b6c
Rachel Waterman <rwaterman@adobe.com>
2024-04-16 14:14:54 +0100

    397: More BEM, fix issue with content headline not showing.

--- aemedge/blocks/fast-facts/fast-facts.js
+++ aemedge/blocks/fast-facts/fast-facts.js
@@ -4,33 +4,33 @@ import {
 
 export default async function decorateBlock(block) {
   const oddCount = block.childElementCount % 2 !== 0;
-  const factsList = ul({ class: `fast-facts-items ${oddCount ? 'odd-count' : 'even-count'}` });
+  const factsList = ul({ class: `fast-facts__items ${oddCount ? 'odd-count' : 'even-count'}` });
   [...block.children].forEach((row) => {
     const eyebrow = row.querySelector('h6')?.textContent || '';
     const factMain = row.querySelector('h4')?.textContent || '';
     const factUnit = row.querySelector('h4 + h5')?.textContent || '';
-    const headline = row.querySelector('em')?.textContent || '';
+    const headline = row.querySelector('strong')?.textContent || '';
     const text = row.querySelector('p:not(:has(strong)):not(.button-container)')?.textContent || '';
     const link = row.querySelector('.button-container') || '';
     if (link) {
-      link.classList.add('fast-facts-item__link');
+      link.classList.add('fast-facts__item__link');
     }
 
     const hasDetails = headline || text || eyebrow;
 
     row.remove();
     factsList.append(li(
-      { class: 'fast-facts-item' },
+      { class: 'fast-facts__item' },
       factMain ? div(
-        { class: 'fast-facts-item__fact' },
-        span({ class: 'fast-facts-item__fact-main' }, factMain),
-        span({ class: 'fast-facts-item__fact-unit' }, factUnit),
+        { class: 'fast-facts__item__fact' },
+        span({ class: 'fast-facts__item__fact-main' }, factMain),
+        span({ class: 'fast-facts__item__fact-unit' }, factUnit),
       ) : '',
       hasDetails ? div(
-        { class: 'fast-facts-item__details' },
-        eyebrow ? div({ class: 'fast-facts-item__eyebrow' }, eyebrow) : '',
-        headline ? div({ class: 'fast-facts-item__headline' }, headline) : '',
-        text ? div({ class: 'fast-facts-item__text' }, text) : '',
+        { class: 'fast-facts__item__details' },
+        eyebrow ? div({ class: 'fast-facts__item__eyebrow' }, eyebrow) : '',
+        headline ? div({ class: 'fast-facts__item__headline' }, headline) : '',
+        text ? div({ class: 'fast-facts__item__text' }, text) : '',
       ) : '',
       link,
     ));
