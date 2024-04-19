822c307edf12d77aeef52bf49c2ad2fa28235a89
rjwtrmn <43483945+rjwtrmn@users.noreply.github.com>
2024-04-19 09:09:03 +0100

    355: TOC improvements (#482)
    * 397: Implemented fast facts block. Moved common link styles from quote stylesheet to global styles, so that they can be used without duplication in fast facts.
    
    * 397: Avoid unnecessary specificity.
    
    * 397: More BEM, fix issue with content headline not showing.
    
    * 397: Increase specificity for link styling again.
    
    * 397: Tweaks to link styles.
    
    * 355: TOC improvements - Hover styling, Nested items for H3, Styling tweaks to align with Frontify, Heading from metadata or placeholders, Restrict header query to default content blocks outside of TOC/sidebar/hero/footer.
    
    * 355: Ensure TOC item extends full length of article page until footer.
    
    * 355: TOC mobile version.
    
    * 355: Small correction to CSS syntax.
    
    ---------
    
    Co-authored-by: Rachel Waterman <rwaterman@adobe.com>
    Co-authored-by: Saurabh Khare <skhare@adobe.com>

--- aemedge/blocks/toc/toc.js
+++ aemedge/blocks/toc/toc.js
@@ -1,38 +1,124 @@
 import {
-  h2, ol, li, span, a, aside,
+  h2, ol, li, span, a, aside, div, button,
 } from '../../scripts/dom-builder.js';
+import { fetchPlaceholders, getMetadata, toCamelCase } from '../../scripts/aem.js';
 
 function setActiveLink() {
+  const selected = document.querySelector('.toc .toc__selected');
+  const selectedLabel = selected.querySelector('span');
+  selected.setAttribute('aria-expanded', 'false');
+
   const links = document.querySelectorAll('.toc li');
   links.forEach((link) => {
-    const linkHash = link.querySelector('a')?.hash;
-    if (linkHash === window.location.hash) {
+    const anchor = link.querySelector('a');
+    const linkHash = anchor?.hash;
+    const isH3 = anchor?.classList.contains('toc__h3-link');
+    const linkMatch = linkHash === window.location.hash;
+    if (linkMatch) {
+      selectedLabel.innerHTML = anchor.innerText;
       link.setAttribute('aria-current', 'true');
+      if (isH3) {
+        link.closest('.toc__list-item.parent').setAttribute('aria-expanded', 'true');
+      } else if (link.classList.contains('parent')) {
+        link.setAttribute('aria-expanded', 'true');
+      }
     } else {
       link.setAttribute('aria-current', 'false');
+      link.setAttribute('aria-expanded', 'false');
+    }
+  });
+}
+
+function buildListItemLink(header) {
+  return a({
+    href: `#${header.id}`,
+    class: `toc__${header.tagName.toLowerCase()}-link`,
+  }, header.innerText);
+}
+
+function buildListItemContent(header) {
+  return div(
+    { class: 'toc__list-item__content' },
+    span({ class: 'toc__list-item__spacer' }),
+    buildListItemLink(header),
+  );
+}
+
+function buildListItem(header, subheaders) {
+  const hasSubheaders = subheaders && subheaders.length > 0;
+  return li(
+    { class: `toc__list-item ${hasSubheaders ? 'parent' : ''}` },
+    buildListItemContent(header),
+    hasSubheaders ? ol(
+      { class: 'toc__list' },
+      ...subheaders.map(
+        (subheader) => li(
+          { class: 'toc__list-item' },
+          buildListItemContent(subheader),
+        ),
+      ),
+    ) : '',
+  );
+}
+
+function buildList(headers) {
+  const tocList = ol({ class: 'toc__list', id: 'toc' });
+  let currentParentHeader;
+  let subheaders = [];
+  headers.forEach((header, index) => {
+    const isH2 = header.tagName === 'H2';
+    if (isH2 && currentParentHeader) {
+      tocList.appendChild(buildListItem(currentParentHeader, subheaders));
+      currentParentHeader = header;
+      subheaders = [];
+    } else if (isH2) {
+      currentParentHeader = header;
+    } else {
+      subheaders.push(header);
+    }
+    if (index === headers.length - 1) {
+      tocList.appendChild(buildListItem(currentParentHeader, subheaders));
+    }
+  });
+  return tocList;
+}
+
+function addClickHandlerToSelectedItem(selected) {
+  selected.addEventListener('click', () => {
+    const listHidden = selected.getAttribute('aria-expanded') === 'false';
+    if (listHidden) {
+      selected.setAttribute('aria-expanded', 'true');
+    } else {
+      selected.setAttribute('aria-expanded', 'false');
     }
   });
 }
 
 export default async function decorate(block) {
-  const mainContent = document.querySelector('main > :nth-child(3)');
-  const headers = mainContent?.querySelectorAll('h2, h3');
+  const mainContent = document.querySelectorAll(
+    'main > .section:not(.hero-container, .toc-container, .section[data-location="sidebar"], .section[data-location="document-footer"]) .default-content-wrapper',
+  );
+  const headers = mainContent ? Array.from(mainContent).reduce((acc, currentSection) => {
+    acc.push(...currentSection.querySelectorAll('h2, h3'));
+    return acc;
+  }, []) : [];
   if (headers.length > 0) {
-    const tocElement = aside({ class: 'toc' }, h2('What\'s on this page'));
-    const tocList = ol();
-    headers.forEach((header) => {
-      const entry = li(
-        span(),
-        a({
-          href: `#${header.id}`,
-          class: `toc-${header.tagName.toLowerCase()}`,
-        }, header.innerText),
-      );
-      tocList.appendChild(entry);
-    });
-    tocElement.appendChild(tocList);
+    const placeholders = await fetchPlaceholders();
+    const heading = getMetadata('toc-heading') || placeholders[toCamelCase('toc-heading')];
+    const selected = button({
+      class: 'toc__selected', 'aria-expanded': 'false', 'aria-haspopup': 'true', 'aria-controls': 'toc', 'aria-label': 'Table of Contents',
+    }, span(heading));
+    const tocElement = aside(
+      {
+        class: 'toc', role: 'navigation', 'aria-label': 'In page',
+      },
+      h2(heading),
+      selected,
+      buildList(headers),
+    );
     block.append(tocElement);
-    window.addEventListener('load', setActiveLink);
+    setActiveLink();
+    addClickHandlerToSelectedItem(selected);
     window.addEventListener('hashchange', setActiveLink);
   }
 }
