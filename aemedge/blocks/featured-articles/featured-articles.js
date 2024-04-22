59ee62823b92568cd613cd283304fff90f9e7115
Rachel Waterman <rwaterman@adobe.com>
2024-04-22 15:07:11 +0100

    411: Load CSS for picture card at top of block decorate method.

--- aemedge/blocks/featured-articles/featured-articles.js
+++ aemedge/blocks/featured-articles/featured-articles.js
@@ -1,4 +1,6 @@
-import { fetchPlaceholders, getMetadata, toCamelCase } from '../../scripts/aem.js';
+import {
+  fetchPlaceholders, getMetadata, loadCSS, toCamelCase,
+} from '../../scripts/aem.js';
 import { ul } from '../../scripts/dom-builder.js';
 import PictureCard from '../../libs/pictureCard/pictureCard.js';
 import { formatDate, fetchPages } from '../../scripts/utils.js';
@@ -21,6 +23,7 @@ function getPictureCard(article, placeholders, authEntry, eager) {
 }
 
 export default async function decorateBlock(block) {
+  loadCSS(`${window.hlx.codeBasePath}/libs/pictureCard/pictureCard.css`);
   const horizontal = block.classList.contains('horizontal');
   const links = Array.from(block.querySelectorAll('a')).map((link) => new URL(link.href).pathname);
   if (links.length > 0) {
@@ -31,7 +34,7 @@ export default async function decorateBlock(block) {
     const cardList = ul();
     Array.from(articles).forEach((article, index) => {
       const card = getPictureCard(article, placeholders, authorEntryFromAuthor(getMetadata('author', article), authEntries), index === 0);
-      cardList.append(card.render(horizontal));
+      cardList.append(card.render(horizontal, true));
     });
     block.append(cardList);
   }
