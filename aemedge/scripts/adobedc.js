ee27fbcc71b4e28e26ae50cb1cd8288b6dfe3ca8
Patrick Hupe <hupe@adobe.com>
2024-04-24 13:44:21 +0200

    fix: L1 case 3: TrustArc only

--- aemedge/scripts/adobedc.js
+++ aemedge/scripts/adobedc.js
@@ -19,14 +19,14 @@ async function sendAdobeDCBeacon(stl = null) {
 
 async function loadAdobeDC() {
   const adobeTagsSrc = {
-    dev: 'https://assets.adobedtm.com/ccc66c06b30b/e990b53307a2/launch-c19417994e2a-development.min.js',
-    stage: 'https://assets.adobedtm.com/ccc66c06b30b/e990b53307a2/launch-c19417994e2a-development.min.js',
-    prod: 'https://assets.adobedtm.com/ccc66c06b30b/e990b53307a2/launch-c19417994e2a-development.min.js',
+    dev: 'https://assets.adobedtm.com/ccc66c06b30b/80d63c1e86f6/launch-5821b00e7b13-development.min.js',
+    stage: 'https://assets.adobedtm.com/ccc66c06b30b/80d63c1e86f6/launch-5821b00e7b13-development.min.js',
+    prod: 'https://assets.adobedtm.com/ccc66c06b30b/80d63c1e86f6/launch-5821b00e7b13-development.min.js',
   };
   const envType = getEnvType();
   if (envType && adobeTagsSrc[envType]) {
     await loadScript(adobeTagsSrc[envType], {});
-    window.console.log(`#L1-2: AdobeDC loaded at ${Date.now() - window.adobeDCStart}ms`);
+    window.console.log(`#L1-3: AdobeDC loaded at ${Date.now() - window.adobeDCStart}ms`);
   }
 }
 
