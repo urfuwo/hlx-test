2218f54b432556cd12344eb739111ff250f8252b
Eduard Seifert <1193638+eduardseifert@users.noreply.github.com>
2024-04-26 18:18:13 +0200

    #514: feat: Top Tips semantically correct markup (WIP)

--- aemedge/blocks/top-tips/top-tips.js
+++ aemedge/blocks/top-tips/top-tips.js
@@ -1,6 +1,40 @@
-import { decorateButtons } from '../../scripts/aem.js';
+import { decorateIcons } from '../../scripts/aem.js';
+import { div, h1, span } from '../../scripts/dom-builder.js';
+
+/**
+ * Formats a string to be more readable by capitalizing the first letter of each word.
+ * @param str The string to format
+ * @returns {*} The formatted string
+ */
+function getReadableBlockName(str) {
+  const parts = str.split('-');
+  const formattedParts = parts.map((part) => part.charAt(0)
+    .toUpperCase() + part.slice(1));
+  return formattedParts.join(' ');
+}
 
 export default async function decorate(block) {
-  console.log(block);
-  decorateButtons(block);
+  const content = block.querySelector('div:nth-child(1)');
+  const blockName = block.getAttribute('data-block-name');
+  const blockHeadline = getReadableBlockName(blockName);
+  const headline = div(
+    {
+      class: 'default-content-wrapper',
+    },
+    h1(
+      {
+        id: blockName,
+      },
+      blockHeadline,
+    ),
+  );
+  const lightBulb = span(
+    {
+      class: ['icon', 'icon-idea'],
+    },
+  );
+
+  content.prepend(headline);
+  block.append(lightBulb);
+  decorateIcons(block);
 }
